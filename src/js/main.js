f43340dd0d425580186e520870ae09cebbc30c74
Johnny Huynh <johnnyhuy@users.noreply.github.com>
2017-09-24 14:16:01 +1000

    Added url-loader, file-loader for importing url() files; Progress changes to main html and minor bug fixes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
-alert('test');
-$('body');
+import ScrollMagic from 'scrollmagic';
+import 'animation.gsap'
+import {TweenMax, Power2, TimelineLite} from 'gsap';
+
+const controller = new ScrollMagic.Controller();
+
+$(document).ready(() => {
+    var cloudTween = new TimelineMax()
+        .to(".farm__cloud_right", 1, {right: 300})
+        .to(".farm__cloud_left", 1, {left: 300}, 0)
+
+    new ScrollMagic.Scene({triggerElement: ".chicken__trigger", duration: 300})
+        .setTween(cloudTween)
+        .setPin(".chicken__image")
+        .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: ".chicken__trigger", duration: 550, offset: 400})
+        .setPin(".chicken__image")
+        .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: ".chicken__trigger", duration: 700, offset: 1100})
+        .setPin(".chicken__image")
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: ".chicken-box__trigger", duration: 800})
+        .setPin(".chicken-box__image")
+        .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: ".chicken-box__trigger", duration: 900, offset: 900})
+        .setPin(".truck__image")
+        .addTo(controller);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 // Require
-const path = require('path');
+const Path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const WebpackNotifierPlugin = require('webpack-notifier');
-const webpack = require('webpack');
+const Webpack = require('webpack');
 
 // Plugins
 const extractSass = new ExtractTextPlugin({
     filename: "[name].css",
     allChunks: true
 });
-const notifier = new WebpackNotifierPlugin()
-const jQuery = new webpack.ProvidePlugin({
+const notifier = new WebpackNotifierPlugin({
+    alwaysNotify: true
+})
+const jQuery = new Webpack.ProvidePlugin({
     $: "jquery",
     jquery: "jQuery",
     "windows.jQuery": "jquery"
 });
-const uglify = new webpack.optimize.UglifyJsPlugin({
-    compress: { warnings: false }
+const uglify = new Webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
 })
 
 module.exports = {
     entry: ['./src/js/main.js', './src/sass/main.scss'],
     output: {
-        path: path.resolve(__dirname, 'src/assets'),
-        filename: 'bundle.js'
+        path: Path.resolve(__dirname, 'src/assets'),
+        filename: 'bundle.js',
+        publicPath: './'
     },
     module: {
         rules: [{
@@ -39,6 +44,10 @@ module.exports = {
             use: {
                 loader: 'babel-loader'
             }
+        },
+        {
+            test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000' 
         }]
     },
     plugins: [
@@ -46,5 +55,16 @@ module.exports = {
         notifier,
         // uglify,
         jQuery,
-    ]
+    ],
+    resolve: {
+        alias: {
+            "TweenLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
+            "TweenMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
+            "TimelineLite": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
+            "TimelineMax": Path.resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
+            "ScrollMagic": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
+            "animation.gsap": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
+            "debug.addIndicators": Path.resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
+        }
+    }
 };
